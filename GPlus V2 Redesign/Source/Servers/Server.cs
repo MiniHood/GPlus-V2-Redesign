ee3bf60e0f3ec7ac54ba8a29ac1bbf7832ec4bcd
HowNiceOfYou <smellywelly1221@gmail.com>
2025-09-14 03:09:01 +0100

    Add properties and constructor to Server class
    - Added `IP`, `Port`, and `Name` properties to the `Server` class in `Server.cs`.
    - Implemented a constructor to initialize these properties.
    - Removed unnecessary `using` directives in `ServerManager.cs`, which remains empty.

--- GPlus V2 Redesign/Source/Servers/Server.cs
+++ GPlus V2 Redesign/Source/Servers/Server.cs
@@ -8,5 +8,15 @@ namespace GPlus_V2_Redesign.Game.Servers
 {
     internal class Server
     {
+        public string IP;
+        public int Port;
+        public string Name;
+
+        public Server(string ip, int port, string name)
+        {
+            IP = ip;
+            Port = port;
+            Name = name;
+        }
     }
 }
--- GPlus V2 Redesign/Source/Servers/ServerManager.cs
+++ GPlus V2 Redesign/Source/Servers/ServerManager.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GPlus_V2_Redesign.Game.Servers
+﻿namespace GPlus_V2_Redesign.Game.Servers
 {
     internal class ServerManager
     {
