ac55dc7978ab7e306ff5c93be6a94ff1b7f7043d
HowNiceOfYou <smellywelly1221@gmail.com>
2025-09-14 19:03:02 +0100

    Steam API integration

---
+++ GPlus V2 Redesign/Source/Steam.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+namespace GPlus_V2_Redesign.Source
+{
+    public static class Steam
+    {
+        public static async Task<string?> GetSteamUsernameAsync(string steamId64)
+        {
+            using var http = new HttpClient();
+            var url = $"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key={SettingsManager.CurrentSettings.General.SteamAPIKey}&steamids={steamId64}";
+            var json = await http.GetStringAsync(url);
+            using var doc = JsonDocument.Parse(json);
+
+            var players = doc.RootElement
+                             .GetProperty("response")
+                             .GetProperty("players");
+
+            if (players.GetArrayLength() == 0) return null; 
+            return players[0].GetProperty("personaname").GetString();
+        }
+    }
+}
