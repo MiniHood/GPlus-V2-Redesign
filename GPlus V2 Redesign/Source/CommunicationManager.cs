d0c78b538f0d57906ffbd94912df8f53b3ec5565
HowNiceOfYou <smellywelly1221@gmail.com>
2025-09-14 19:02:29 +0100

    Added communication manager

---
+++ GPlus V2 Redesign/Source/CommunicationManager.cs
@@ -0,0 +1,22 @@
+﻿using GPlus_V2_Redesign.Game.Clients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static GPlus_V2_Redesign.Source.Memory;
+
+namespace GPlus_V2_Redesign.Source
+{
+    internal static class CommunicationManager
+    {
+        public static void ParseCommunication(Message m)
+        {
+            COPYDATASTRUCT cds = (COPYDATASTRUCT)Marshal.PtrToStructure(m.LParam, typeof(COPYDATASTRUCT));
+            SteamMessage msg = (SteamMessage)Marshal.PtrToStructure(cds.lpData, typeof(SteamMessage));
+            ClientManager.AttemptSync(msg);       
+        }
+
+    }
+}
