ab16bfbd41a4601d8f505f6c426c80042c4f660d
HowNiceOfYou <smellywelly1221@gmail.com>
2025-09-14 03:08:51 +0100

    Refactor Client class and simplify ClientManager
    Updated Client.cs with new properties, methods, and using directives for game servers and networking. Introduced asynchronous connection monitoring and improved constructor logic. Simplified ClientManager.cs by removing unnecessary directives, leaving it as a placeholder for future development.

--- GPlus V2 Redesign/Source/Clients/Client.cs
+++ GPlus V2 Redesign/Source/Clients/Client.cs
@@ -1,14 +1,33 @@
-﻿using GPlus_V2_Redesign.Source;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GPlus_V2_Redesign.Game.Servers;
+using GPlus_V2_Redesign.Source;
+using GPlus_V2_Redesign.Source.Network;
+using GPlus_V2_Redesign.Source.Sandboxie;
 
 namespace GPlus_V2_Redesign.Game.Clients
 {
     internal class Client
     {
-    
+        public LoginDetails LoginDetails;
+        public Server? ConnectedServer;
+        public required Sandboxie Enviroment;
+        public bool IsConnected => ConnectedServer != null;
+
+        private async Task ClientLoop()
+        {
+            while (true) {
+                // Constantly check status on connection
+                Enviroment._rconConnection
+
+                await Task.Delay(100);
+            }
+        }
+
+        public Client(LoginDetails loginDetails)
+        {
+            LoginDetails = loginDetails;
+            Enviroment = SandboxieManager.CreateNewSandbox(LoginDetails);
+            ConnectedServer = null;
+        }
+
     }
 }
--- GPlus V2 Redesign/Source/Clients/ClientManager.cs
+++ GPlus V2 Redesign/Source/Clients/ClientManager.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GPlus_V2_Redesign.Game.Clients
+﻿namespace GPlus_V2_Redesign.Game.Clients
 {
     internal class ClientManager
     {
