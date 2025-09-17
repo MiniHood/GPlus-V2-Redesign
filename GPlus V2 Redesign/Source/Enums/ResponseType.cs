d71fee1ee1ca413f669526e21193a9a5b8c3fd63
HowNiceOfYou <smellywelly1221@gmail.com>
2025-09-17 04:49:54 +0100

    Added ResponeTypes

---
+++ GPlus V2 Redesign/Source/Enums/ResponseType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPlus.Source.Enums
+{
+    public enum ResponseType
+    {
+        Unknown = 0,
+        Verifying = 1,
+        Downloading = 2,
+        Commiting = 3,
+    }
+}
