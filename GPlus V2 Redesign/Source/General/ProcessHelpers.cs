87bfea2f0ad500b3bb722a40aac4ecbc5cb6ceca
HowNiceOfYou <smellywelly1221@gmail.com>
2025-09-18 22:37:14 +0100

    added process helpers

---
+++ GPlus V2 Redesign/Source/General/ProcessHelpers.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPlus.Source.General
+{
+    internal static class ProcessHelpers
+    {
+        internal static IEnumerable<Process> GetChildProcessesRecursive(int parentId)
+        {
+            var children = new List<Process>();
+            try
+            {
+                using var searcher = new ManagementObjectSearcher(
+                    $"SELECT ProcessId FROM Win32_Process WHERE ParentProcessId={parentId}");
+                foreach (ManagementObject mo in searcher.Get())
+                {
+                    int pid = Convert.ToInt32(mo["ProcessId"]);
+                    try
+                    {
+                        var childProc = Process.GetProcessById(pid);
+                        children.Add(childProc);
+                        // Recursively get this child's children
+                        children.AddRange(GetChildProcessesRecursive(pid));
+                    }
+                    catch { /* process may have exited */ }
+                }
+            }
+            catch { }
+            return children;
+        }
+
+        internal static string GetCommandLine(Process process)
+        {
+            try
+            {
+                using var searcher = new ManagementObjectSearcher(
+                    $"SELECT CommandLine FROM Win32_Process WHERE ProcessId = {process.Id}");
+                foreach (ManagementObject obj in searcher.Get())
+                {
+                    return obj["CommandLine"]?.ToString() ?? "";
+                }
+            }
+            catch { }
+            return "";
+        }
+    }
+}
