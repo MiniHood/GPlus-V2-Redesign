613a341bc3d88016baeceb8a4aa5c72849462c4c
HowNiceOfYou <smellywelly1221@gmail.com>
2025-09-18 22:37:05 +0100

    added gmod request types

---
+++ GPlus V2 Redesign/Source/Enums/GMODRequestTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPlus.Source.Enums
+{
+    internal enum GMODRequestTypes
+    {
+        QUIT = 0,
+        PING = 1,
+    }
+}
