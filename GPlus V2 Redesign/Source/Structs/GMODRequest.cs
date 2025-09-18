f2bbfa386d72c6dbc4bc4c89780c2234edd496ff
HowNiceOfYou <smellywelly1221@gmail.com>
2025-09-18 22:40:33 +0100

    added gmod request struct

---
+++ GPlus V2 Redesign/Source/Structs/GMODRequest.cs
@@ -0,0 +1,25 @@
+﻿using GPlus.Source.Enums;
+using System;
+using System.Text.Json;
+
+namespace GPlus.Source.Structs
+{
+    internal class GMODRequest
+    {
+        public Guid RequestId { get; set; } = Guid.NewGuid();
+        public GMODRequestTypes Type { get; set; }
+        public bool ExpectResponse { get; set; }
+        public string[] Args { get; set; } = Array.Empty<string>();
+
+        public string ToJson() => JsonSerializer.Serialize(this);
+
+        public static GMODRequest FromJson(string json) =>
+            JsonSerializer.Deserialize<GMODRequest>(json)!;
+    }
+
+    internal class GMODResponse
+    {
+        public Guid RequestId { get; set; }
+        public string Result { get; set; }
+    }
+}
