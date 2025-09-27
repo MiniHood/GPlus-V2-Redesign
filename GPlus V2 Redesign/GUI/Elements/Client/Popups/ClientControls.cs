db24c73f6b1635518c9c76b0ff05bd80af46f83e
HowNiceOfYou <smellywelly1221@gmail.com>
2025-09-27 07:17:37 +0100

    implemented basic client control logic

--- GPlus V2 Redesign/GUI/Elements/Client/Popups/ClientControls.cs
+++ GPlus V2 Redesign/GUI/Elements/Client/Popups/ClientControls.cs
@@ -4,23 +4,51 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using GPlus.Game.Clients;
+using GPlus.Source.Interprocess;
 namespace GPlus.GUI.Elements.Client.Popups
 {
     public partial class ClientControls : UserControl
     {
-        internal GPlus.Game.Clients.Client SelectedClient;
+        internal static Game.Clients.Client SelectedClient;
         public ClientControls()
         {
             InitializeComponent();
         }
 
+        internal async void RefreshUI()
+        {
+            if (SelectedClient == null) return;
+
+            // Update labels, textboxes, etc.
+            _lblUsername.Text = SelectedClient.LoginDetails.Username;
+            if(SelectedClient.GMOD.Process == null)
+            {
+                _lblProcessID.Text = $"Process ID: Starting...";
+                _lblOnWebsocket.Text = $"Is Lua Ready: {(SelectedClient.GMOD.LuaReady ? "Yes" : "No")}";
+            } else
+            {
+                _lblProcessID.Text = $"Process ID: {SelectedClient.GMOD.Process.Id.ToString()}";
+                _lblOnWebsocket.Text = $"Is Lua Ready: {(SelectedClient.GMOD.LuaReady ? "Yes" : "No")}";
+            }
+        }
+
         private void ClientControls_Load(object sender, EventArgs e)
         {
-            
+            if (DesignMode) return;
+
+        }
+
+        private async void _btnTestLua_Click(object sender, EventArgs e)
+        {
+            if (SelectedClient == null)
+                return;
+
+            await SelectedClient.GMOD.SendPrintTest();
         }
     }
 }
