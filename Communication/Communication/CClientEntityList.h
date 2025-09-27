17844535462fe5a1f9c013934f127cc79ceb0592
HowNiceOfYou <smellywelly1221@gmail.com>
2025-09-27 07:13:50 +0100

    added ccliententitylist header

---
+++ Communication/Communication/CClientEntityList.h
@@ -0,0 +1,15 @@
+#pragma once
+// All credits to https://github.com/Gaztoof/GMod-SDK 
+class CClientEntityList
+{
+public:
+	/*0*/	virtual void* Constr(void*) = 0;
+	/*2*/	virtual void* Destr1() = 0;
+	/*3*/	virtual void* destr2() = 0;
+	/*4*/	virtual void* GetClientEntity(int entnum) = 0;
+	/*5*/	virtual void* GetClientEntityFromHandle(uintptr_t hEnt) = 0;
+	/*6*/	virtual int NumberOfEntities(bool bIncludeNonNetworkable = false) = 0;
+	/*10*/	virtual int GetHighestEntityIndex(void) = 0;
+	/*11*/	virtual void SetMaxEntities(int maxents) = 0;
+	/*12*/	virtual int GetMaxEntities(void) = 0;
+};
